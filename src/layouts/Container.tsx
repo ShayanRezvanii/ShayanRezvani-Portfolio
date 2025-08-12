ab1cadbd6d8946184243d5bb12ea4572885bb372
shayanrezvanii <shayanrezvani5@gmail.com>
2025-08-12 19:06:53 +0330

    update

--- src/layouts/Container.tsx
+++ src/layouts/Container.tsx
@@ -5,7 +5,7 @@ interface ContainerType {
 }
 function Container({ children }: ContainerType) {
   return (
-    <div className="w-full min-h-screen font-quicksand lg:h-screen bg-[#F2F5F9] dark:bg-gray-900 sm:overflow-x-hidden  max-w-[1440px] py-4 mx-auto">
+    <div className="w-full min-h-screen font-quicksand lg:h-screen bg-[#F2F5F9] dark:bg-gray-900 overflow-x-hidden md:overflow-x-scroll  max-w-[1440px] py-4 mx-auto">
       {children}
     </div>
   );
